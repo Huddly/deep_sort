eb295916916488a40ce92c5a7f8dacbbbc1050df
Andard <anders.r@huddly.com>
2019-08-29 16:45:19 +0200

    split up code in functions

--- videos.py
+++ videos.py
@@ -4,6 +4,8 @@ import numpy as np
 import warnings
 import os
 from pathlib import Path
+import argparse
+from tqdm import tqdm
 
 from deep_sort.tracker import Tracker
 from deep_sort import nn_matching
@@ -12,113 +14,170 @@ from face_recognition import face_encodings
 from bbox import AnnotationInstance, AnnotationEntry, AnnotationContainer, DatasetSourceProvider
 from bbox.instances import BBox
 from bbox.contrib.detection.darknet import DarknetObjectDetector
-    
+
+width = 0
+height = 0
+
 model_path = sys.argv[1]
 vid_path = sys.argv[2]
 out_path = sys.argv[3]
 warnings.filterwarnings('ignore')
 
-cfg = list(Path(model_path).glob('*.cfg'))
-weights = list(Path(model_path).glob('*.weights'))
-data = list(Path(model_path).glob('*.data'))
-names = list(Path(model_path).glob('names.txt'))
+def get_darknet_options(model_path):
 
-assert len(cfg) > 0, f'Unable to locate {Path(model_path) / "[MODEL].cfg"}'
-assert len(weights) > 0, f'Unable to locate {Path(model_path) / "[MODEL].weights"}'
-assert len(data) > 0, f'Unable to locate {Path(model_path) / "[MODEL].data"}'
-assert len(names) > 0, f'Unable to locate {Path(model_path) / "names.txt"}: Check path in [MODEL].data'
+    cfg = list(Path(model_path).glob('*.cfg'))
+    weights = list(Path(model_path).glob('*.weights'))
+    data = list(Path(model_path).glob('*.data'))
+    names = list(Path(model_path).glob('names.txt'))
 
-dssp = DatasetSourceProvider()
-dssp.add_folder(folder_path=vid_path, dataset_name='playtime')
-dssp_tracks = DatasetSourceProvider()
-dssp_tracks.add_folder(folder_path=vid_path, dataset_name='tracks')
+    assert len(cfg) > 0, f'Unable to locate {Path(model_path) / "[MODEL].cfg"}'
+    assert len(weights) > 0, f'Unable to locate {Path(model_path) / "[MODEL].weights"}'
+    assert len(data) > 0, f'Unable to locate {Path(model_path) / "[MODEL].data"}'
+    assert len(names) > 0, f'Unable to locate {Path(model_path) / "names.txt"}: Check path in [MODEL].data'
 
-container = AnnotationContainer(dataset_source_provider=dssp, dataset_version='0.0.1')
-container_tracks = AnnotationContainer(dataset_source_provider=dssp_tracks, dataset_version='0.0.1')
+    return cfg, weights, data, names
 
-dn_path = '/home/anders/huddly/libs/darknet'
-with DarknetObjectDetector(Path(dn_path).parents[0] / 'darknet',
+
+def make_containers(vid_path):
+
+    dssp = DatasetSourceProvider()
+    dssp.add_folder(folder_path=vid_path, dataset_name='playtime')
+    dssp_tracks = DatasetSourceProvider()
+    dssp_tracks.add_folder(folder_path=vid_path, dataset_name='tracks')
+
+    container = AnnotationContainer(dataset_source_provider=dssp, dataset_version='0.0.1')
+    container_tracks = AnnotationContainer(dataset_source_provider=dssp_tracks, dataset_version='0.0.1')
+
+    return container, container_tracks
+
+def add_tracks(entry, info):
+    b = info['bbox']
+    t_id = info['id']
+    bb = BBox(xmin=np.clip(int(b[0]), 0, width), ymin=np.clip(int(b[1]), 0, height), xmax=int(b[0]+b[2]), ymax=int(b[1]+b[3]),
+            label=str(t_id),
+            coordinate_mode='ABSOLUTE')
+    
+    entry.add_instance(AnnotationInstance(parent=entry, bbox=bb))
+    return bb, entry
+
+def add_instance(entry, instance):
+    instance.parent = entry
+    instance.bbox.convert_coordinate_mode(target_mode='ABSOLUTE', image_size=entry.image_size)
+    entry.add_instance(instance)
+    
+    return instance, entry
+
+def get_tracker(n_init=4, max_cosine_distance=0.2):
+    metric = nn_matching.NearestNeighborDistanceMetric(
+        "cosine", 0.2, None)
+    tracker = Tracker(metric, n_init=4)
+    return tracker
+
+def get_track_info(track):
+    return {'bbox': track.to_tlwh(), 'id': track.track_id, 'hits': track.hits}
+
+def draw(frame):
+    image = cv2.resize(frame, (1920, 1080))
+    cv2.imshow('Pred', image)
+    return cv2.waitKey(1)
+
+def process_image(tracker, img_nr, video_path, container, container_tracks):
+    global height
+    global width
+    result = []
+    image = str(img_nr) + '.jpg'
+    frame = cv2.imread(str(video_path / image))
+    height, width, _ = frame.shape
+    entry = AnnotationEntry(parent=container, image_name=image, image_size=(width, height),
+                            dataset_name='playtime', dataset_subset='in')
+    entry_tracks = AnnotationEntry(parent=container_tracks, image_name=image, image_size=(width, height),
+                            dataset_name='tracks', dataset_subset='in')
+
+    for track in tracker.tracks:
+        if not track.is_confirmed() or track.time_since_update > 2:
+            continue  
+        result.append(get_track_info(track))
+    
+    return frame, entry, entry_tracks, result
+
+def update_detection_and_tracker(frame, tracker, od):
+    height, width, ch = frame.shape
+    pred = od.detect_image(frame)
+    detections = []
+    for inst in pred:
+        if inst.label == 'head':
+            crop = frame[int(inst.ymin*height):int(inst.ymax*height), int(inst.xmin*width):int(inst.xmax*width)]
+            feat = face_encodings(crop)
+            detections.append(Detection((int(inst.xmin*width), int(inst.ymin*height), int((inst.xmax-inst.xmin)*width),
+                                        int((inst.ymax-inst.ymin)*height)),
+                                        inst.score,
+                                        list(feat[0]) if len(feat) > 0 else [1]*128))
+    tracker.predict()
+    tracker.update(detections)
+
+    return tracker, pred
+
+def main(args):
+    vid_path = args.VIDEO_PATH
+    container, container_tracks = make_containers(vid_path)
+
+    dn_path = args.DARKNET_PATH
+    show = args.show
+
+    cfg, weights, data, names = get_darknet_options(args.MODEL)
+    with DarknetObjectDetector(
+                            Path(dn_path).parents[0] / 'darknet',
                             cfg_path=cfg[0],
                             weights_path=weights[0],
                             data_file_path=data[0],
-                            detection_score_threshold=0.3) as od:
+                            detection_score_threshold=0.4) as od:
+        i = 0
+        first = True
+        video_path = Path(vid_path) / 'in'
+        max_frame = len(list(Path(video_path).glob('*.jpg')))
+        tracker = get_tracker(n_init=4)
 
-    metric = nn_matching.NearestNeighborDistanceMetric(
-        "cosine", 0.2, None)
-    tracker = Tracker(metric, n_init=5)
-    
-    i = 0
-    first = True
-    video_path = Path(vid_path) / 'in'
-    max_frame = len(list(Path(video_path).glob('*.jpg')))
-    entries = []
+        for img_nr in tqdm(range(max_frame)):
+            frame, entry, entry_tracks, result = process_image(tracker, img_nr, video_path, container, container_tracks)
 
-    for img_nr in range(max_frame):
-        result = []
-        image = str(img_nr) + '.jpg'
-        frame = cv2.imread(str(video_path / image))
-        height, width, ch = frame.shape
-        entry = AnnotationEntry(parent=container, image_name=image, image_size=(width, height),
-                                dataset_name='playtime', dataset_subset='in')
-        entry_tracks = AnnotationEntry(parent=container_tracks, image_name=image, image_size=(width, height),
-                                dataset_name='tracks', dataset_subset='in')
-        
-        for track in tracker.tracks:
-            if not track.is_confirmed() or track.time_since_update > 2:
-                continue
-            info = {'bbox': track.to_tlwh(), 'id': track.track_id, 'hits': track.hits}
-            result.append(info)
-
-        if (i % 22 == 0) or first:
-            i = 0
-            pred = od.detect_image(frame)
-            detections = []
-            for inst in pred:
-                if inst.label == 'person':
-                    crop = frame[int(inst.ymin*height):int(inst.ymax*height), int(inst.xmin*width):int(inst.xmax*width)]
-                    feat = face_encodings(crop)
-                    detections.append(Detection((int(inst.xmin*width), int(inst.ymin*height), int((inst.xmax-inst.xmin)*width),
-                                                int((inst.ymax-inst.ymin)*height)),
-                                                inst.score,
-                                                list(feat[0]) if len(feat) > 0 else [1]*128))
-            first = False
-            tracker.predict()
-            tracker.update(detections)
-        
-        
-        for inf in result:
-            b = inf['bbox']
-            t_id = inf['id']
-            bb = BBox(xmin=np.clip(int(b[0]), 0, width), ymin=np.clip(int(b[1]), 0, height), xmax=int(b[0]+b[2]), ymax=int(b[1]+b[3]),
-                    label=str(t_id),
-                    coordinate_mode='ABSOLUTE')
+            if True:
+                i = 0
+                tracker, pred = update_detection_and_tracker(frame, tracker, od)
+                first = False
             
-            entry_tracks.add_instance(AnnotationInstance(parent=entry_tracks, bbox=bb))
-            #bb.overlaid_on_image(frame, (200, 200, 200), draw_label=True, draw_score=False)
-            #cv2.rectangle(frame, (int(b[0]), int(b[1])), (int(b[2]+b[0]), int(b[3]+b[1])), (255, 0, 0), 2)
-            # cv2.putText(frame, str(t_id), (int(b[0]), int(b[1])), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-        for inst in pred:
-            inst.parent = entry
-            inst.bbox.convert_coordinate_mode(target_mode='ABSOLUTE', image_size=entry.image_size)
-            entry.add_instance(inst)
-        i += 1
-        #print(entry.get_image_full_path())
-        container.add_entry(entry)
-        container_tracks.add_entry(entry_tracks)
-        #entry.show()
-        for inst in entry:
-            inst.bbox.overlaid_on_image(frame, (200, 200, 200), draw_label=True, draw_score=False)
+            for inf in result:
+                bb, entry_tracks = add_tracks(entry_tracks, inf)
+                if show:
+                    bb.overlaid_on_image(frame, (200, 200, 200), draw_label=True, draw_score=False)
 
-        image = cv2.resize(frame, (1920, 1080))
-        cv2.imshow('Pred', image)
-        key = cv2.waitKey(20)
+            for inst in pred:
+                instance, entry = add_instance(entry, inst)
+                if show:
+                   pass #instance.bbox.overlaid_on_image(frame, (200, 0, 200), draw_label=True, draw_score=False)
 
-        if key == ord('q'):
-            break
+            container.add_entry(entry)
+            container_tracks.add_entry(entry_tracks)
+
+            if show: 
+                key = draw(frame)
+                if key == ord('q'):
+                    break
+            
+            i += 1
+
     cv2.destroyAllWindows()
-    
     container.summary()
     container_tracks.summary()
-    container.write_json(os.path.join(out_path, vid_path.split('/')[-1], '.json'))
-    container_tracks.write_json(os.path.join(out_path, vid_path.split('/')[-1], '_tracks.json'))
-    print('wrote container to /home/anders/huddly/projects/autozoom/scoring/scoring-output/' + vid_path.split('/')[-1] + '.json')
+    container.write_json(os.path.join(args.OUTPUT, vid_path.split('/')[-1] + '.json'))
+    container_tracks.to_file(os.path.join(args.OUTPUT, vid_path.split('/')[-1] + '_tracks.bbox'))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Run detection and tracking on scoring video')
+    parser.add_argument('MODEL', help='path to darknet model')
+    parser.add_argument('VIDEO_PATH', help='Path to "in" folder for video')
+    parser.add_argument('DARKNET_PATH', help='Path to darknet folder')
+    parser.add_argument('OUTPUT', help='output path')
+    parser.add_argument('-s', '--show', help='display detections and tracks on video', action='store_true')
+    args = parser.parse_args()
+    main(args)
